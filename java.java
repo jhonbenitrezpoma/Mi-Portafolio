fe8a37e4301c1e5fecb1539a2a53a38bd48df418
jhonbenitrezpoma <tuhermana1591q@gmail.com>
2026-09-20 23:28:57 -0500

    Create java.java

---
+++ java.java
@@ -0,0 +1,25 @@
+package com.upla.portafolio;
+
+/**
+ * Ejemplo de Código Java para Algoritmos y Estructuras de Datos
+ * Universidad Peruana Los Andes
+ * Estudiante: Jhon Benitrez Poma
+ */
+public class java {
+
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("   PORTAFOLIO ACADÉMICO UPLA - 2026       ");
+        System.out.println("   Desarrollado por: Jhon Benitrez       ");
+        System.out.println("==========================================");
+
+        int[] numeros = {10, 20, 30, 40, 50};
+        int suma = 0;
+
+        for (int num : numeros) {
+            suma += num;
+        }
+
+        System.out.println("La suma del arreglo de prueba es: " + suma);
+    }
+}
